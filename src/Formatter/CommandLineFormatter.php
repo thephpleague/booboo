1e45ca64471ca8f4268a47d6faf2927780350acc
Brandon Savage <brandon@brandonsavage.net>
2014-12-03 17:11:30 -0500

    Adding a command line formatter.

---
+++ src/Formatter/CommandLineFormatter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Savage\ShitHappens\Formatter;
+
+class CommandLineFormatter extends AbstractFormatter {
+
+    public function format(\Exception $e) {
+        if($e instanceof \ErrorException) {
+            return $this->handleErrors($e);
+        }
+
+        return $this->formatExceptions($e);
+    }
+
+    public function handleErrors(\ErrorException $e) {
+        $errorString = "\n ------------------------\n  AN ERROR HAS OCCURRED \n ------------------------\n";
+        $errorString .= " %s: %s in %s on line %d\n";
+
+        $severity = $this->determineSeverityTextValue($e->getSeverity());
+
+        $error = $e->getMessage();
+        $file = $e->getFile();
+        $line = $e->getLine();
+
+        $error = sprintf($errorString, $severity, $error, $file, $line);
+        return $error;
+    }
+
+    protected function formatExceptions(\Exception $e) {
+
+        $errorString = "\n ---------------------------\n  AN EXCEPTION HAS OCCURRED \n ---------------------------\n";
+        $errorString .= " Fatal error: Uncaught exception '%s' with message '%s' in %s on line %d\n\nStack Trace:\n%s\n";
+
+        $type = get_class($e);
+        $message = $e->getMessage();
+        $file = $e->getFile();
+        $line = $e->getLine();
+        $trace = $e->getTraceAsString();
+
+        $error = sprintf($errorString, $type, $message, $file, $line, $trace);
+        return $error;
+    }
+
+}
