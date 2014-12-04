37f9aa84db473e9c6d16b7bee583ddb1657aabfd
Brandon Savage <brandon@brandonsavage.net>
2014-12-04 10:51:07 -0500

    Documenting Runner class.

--- src/Runner.php
+++ src/Runner.php
@@ -7,17 +7,45 @@ use Savage\ShitHappens\Handler;
 
 class Runner
 {
-
+    /**
+     * A constant for the error handling function.
+     */
     const ERROR_HANDLER = 'errorHandler';
+
+    /**
+     * A constant for the exception handler.
+     */
     const EXCEPTION_HANDLER = 'exceptionHandler';
 
+    /**
+     * @var array Handler stack array
+     */
     protected $handlerStack = array();
+
+    /**
+     * @var array Formatter stack array
+     */
     protected $formatterStack = array();
+
+    /**
+     * @var bool Whether or not we should silence all errors.
+     */
     protected $silenceErrors = false;
 
+    /**
+     * @var An error page formatter, for creating pretty error pages in production
+     */
     protected $errorPage;
+
+    /**
+     * @var bool If set to true, will throw all errors as exceptions (making them blocking)
+     */
     protected $throwErrorsAsExceptions = false;
 
+    /**
+     * @param array $formatters
+     * @param array $handlers
+     */
     public function __construct(array $formatters = [], array $handlers = [])
     {
         // Let's honor the INI settings.
@@ -34,6 +62,17 @@ class Runner
         }
     }
 
+    /**
+     * An error handling function for PHP. Follows the protocols laid out
+     * in the documentation for defining an error handler. Variable names
+     * are straight from the PHP documentation.
+     *
+     * @param $errno
+     * @param $errstr
+     * @param $errfile
+     * @param $errline
+     * @throws \ErrorException
+     */
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
         $fatalErrors = E_ERROR | E_USER_ERROR | E_COMPILE_ERROR | E_CORE_ERROR;
@@ -60,6 +99,12 @@ class Runner
         }
     }
 
+    /**
+     * An exception handler, per the documentation in PHP. This function is
+     * also used for the handling of errors, even when they are non-blocking.
+     *
+     * @param \Exception $e
+     */
     public function exceptionHandler(\Exception $e)
     {
         $this->runHandlers($e);
@@ -82,6 +127,12 @@ class Runner
         }
     }
 
+    /**
+     * Registers the error handlers, and is required to be called before the
+     * error handling code is effective.
+     *
+     * @throws Exception\NoFormattersRegisteredException
+     */
     public function register()
     {
         if (empty($this->formatterStack)) {
@@ -94,53 +145,102 @@ class Runner
         set_exception_handler(array($this, self::EXCEPTION_HANDLER));
     }
 
+    /**
+     * Add a new handler to the stack.
+     *
+     * @param Handler\HandlerInterface $handler
+     * @return $this
+     */
     public function pushHandler(Handler\HandlerInterface $handler)
     {
         $this->handlerStack[] = $handler;
         return $this;
     }
 
+    /**
+     * Remove an error handler from the bottom of the stack.
+     *
+     * @return Handler\HandlerInterface|null
+     */
     public function popHandler()
     {
         return array_pop($this->handlerStack);
     }
 
+    /**
+     * Get a list of available handlers.
+     *
+     * @return array
+     */
     public function getHandlers()
     {
         return $this->handlerStack;
     }
 
+    /**
+     * CLear all the handlers.
+     *
+     * @return $this
+     */
     public function clearHandlers()
     {
         $this->handlerStack = array();
         return $this;
     }
 
+    /**
+     * Adds a new formatter to the formatter stack.
+     *
+     * @param Formatter\FormatterInterface $formatter
+     * @return $this
+     */
     public function pushFormatter(Formatter\FormatterInterface $formatter)
     {
         $this->formatterStack[] = $formatter;
         return $this;
     }
 
+    /**
+     * Pops a formatter off the bottom of the formatter stack.
+     *
+     * @return Formatter\FormatterInterface|null
+     */
     public function popFormatter()
     {
         return array_pop($this->formatterStack);
     }
 
+    /**
+     * Gets all formatters currently registered.
+     *
+     * @return array
+     */
     public function getFormatters()
     {
         return $this->formatterStack;
     }
 
+    /**
+     * Clears all formatters currently registered.
+     *
+     * @return $this
+     */
     public function clearFormatters()
     {
         $this->formatterStack = array();
         return $this;
     }
 
+    /**
+     * Runs all the handlers registered, and returns the exception provided.
+     *
+     * @param \Exception $e
+     * @return \Exception
+     */
     protected function runHandlers(\Exception $e)
     {
 
+        /** @var Handler\HandlerInterface $handler */
         foreach (array_reverse($this->handlerStack) as $handler) {
             $handler->handle($e);
         }
@@ -158,6 +258,7 @@ class Runner
             $severity = E_ERROR;
         }
 
+        /** @var Formatter\FormatterInterface $formatter */
         foreach (array_reverse($this->formatterStack) as $formatter) {
             if ($severity & $formatter->getErrorLimit()) {
                 return $formatter->format($e);
@@ -165,11 +266,21 @@ class Runner
         }
     }
 
+    /**
+     * Silences all errors.
+     *
+     * @param $bool
+     */
     public function silenceAllErrors($bool)
     {
         $this->silenceErrors = (bool)$bool;
     }
 
+    /**
+     * Deregisters the error handling functions, returning them to their previous state.
+     *
+     * @return $this
+     */
     public function deregister()
     {
         restore_error_handler();
@@ -177,11 +288,22 @@ class Runner
         return $this;
     }
 
+    /**
+     * Registers an error page for handling uncaught exceptions in production.
+     *
+     * @param Formatter\FormatterInterface $errorPage
+     */
     public function setErrorPageFormatter(Formatter\FormatterInterface $errorPage)
     {
         $this->errorPage = $errorPage;
     }
 
+    /**
+     * Allows the user to explicitly require errors to be thrown as exceptions. This
+     * makes all errors blocking, even if they are minor (e.g. E_NOTICE, E_WARNING).
+     *
+     * @param $bool
+     */
     public function treatErrorsAsExceptions($bool)
     {
         $this->throwErrorsAsExceptions = (bool)$bool;
