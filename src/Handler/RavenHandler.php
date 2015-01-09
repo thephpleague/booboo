3f06f8ab64a5145714608b2daa1703be07ff7ce1
Brandon Savage <brandon@brandonsavage.net>
2015-01-09 16:10:17 -0500

    Adding a handler for Raven and Sentry.

---
+++ src/Handler/RavenHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Savage\BooBoo\Handler;
+
+use \Exception;
+use Savage\BooBoo\Handler\HandlerInterface;
+
+class RavenHandler implements HandlerInterface
+{
+    protected $client;
+
+    public function __construct(\Raven_Client $client, $minimumLogLevel = E_ALL)
+    {
+        $this->client = $client;
+        $this->minimumLogLevel = $minimumLogLevel;
+    }
+
+    public function handle(Exception $e)
+    {
+        if ($e instanceof \ErrorException) {
+            $level = $e->getSeverity();
+        } else {
+            $level = E_ERROR;
+        }
+
+        if($this->minimumLogLevel & $level) {
+            $this->client->getIdent($this->client->captureException($e));
+        }
+    }
+}
