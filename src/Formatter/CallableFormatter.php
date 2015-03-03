422b3f0cd6b52cef710a6901db004eb03f3c4314
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-03-03 21:34:11 +0100

    Added CallableFormatter and CallableHandler for full extensibility

---
+++ src/Formatter/CallableFormatter.php
@@ -0,0 +1,29 @@
+<?php
+namespace League\BooBoo\Formatter;
+
+use Exception;
+
+class CallableFormatter extends AbstractFormatter
+{
+    /**
+     * @var callable
+     */
+    protected $callable;
+
+    /**
+     * @param callable $callable
+     */
+    public function __construct(callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    /**
+     * @param Exception $e
+     * @return mixed
+     */
+    public function format(Exception $e)
+    {
+        return call_user_func($this->callable, $e);
+    }
+}
